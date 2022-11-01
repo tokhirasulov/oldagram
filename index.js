bb885eb1275b261f246f00814c6d029c229b5898
Tokhir Rasulov <rasulovtokhir11@gmail.com>
2022-11-01 18:51:50 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
-
+let firstPhoto = document.getElementById("first-photo")
+let secondPhoto = document.getElementById("second-photo")
+let thirdPhoto = document.getElementById("third-photo")
+let tokhirPhoto = document.getElementById("Tokhir-photo")
 const likeFirst = document.getElementById("like-first")
 let firstLikeNumber = document.getElementById("like-number-first")
 let likeNumberFirst = 945
@@ -11,6 +14,62 @@ let likeNumberThird = 503
 const likeTokhir = document.getElementById("like-Tokhir")
 let tokhirLikeNumber = document.getElementById("like-number-Tokhir")
 let likeNumberTokhir = 999
+
+firstPhoto.addEventListener("dblclick", function(){
+    if (likeFirst.classList.contains("fa-regular")){
+        likeFirst.classList.remove("fa-regular");
+        likeFirst.classList.add("fas");
+    increment()
+    }
+    else if (likeFirst.classList.contains("fas")){
+        likeFirst.classList.remove("fas");
+        likeFirst.classList.add("fa-regular");
+        decrement()
+    }
+
+})
+
+secondPhoto.addEventListener("click", function(){
+    if (likeSecond.classList.contains("fa-regular")){
+        likeSecond.classList.remove("fa-regular");
+        likeSecond.classList.add("fas");
+    incrementSecond()
+    }
+    else if (likeSecond.classList.contains("fas")){
+        likeSecond.classList.remove("fas");
+        likeSecond.classList.add("fa-regular");
+        decrementSecond()
+    }
+})
+
+thirdPhoto.addEventListener("click", function(){
+    if (likeThird.classList.contains("fa-regular")){
+        likeThird.classList.remove("fa-regular");
+        likeThird.classList.add("fas");
+    incrementThird()
+    }
+    else if (likeThird.classList.contains("fas")){
+        likeThird.classList.remove("fas");
+        likeThird.classList.add("fa-regular");
+        decrementThird()
+    }
+})
+
+
+tokhirPhoto.addEventListener("click", function(){
+    if (likeTokhir.classList.contains("fa-regular")){
+        likeTokhir.classList.remove("fa-regular");
+        likeTokhir.classList.add("fas");
+    incrementTokhir()
+    }
+    else if (likeTokhir.classList.contains("fas")){
+        likeTokhir.classList.remove("fas");
+        likeTokhir.classList.add("fa-regular");
+        decrementTokhir()
+    }
+})
+
+
 
 
 
